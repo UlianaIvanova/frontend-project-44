1fb70af67e3c80d8d2db58220189a7ba6a1e3c10
Uliana <uli.a.na.iva.nova@mail.ru>
2024-05-02 16:29:08 +0300

    task

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,17 +1,21 @@
-#!/usr/bin/env node 
-import gameBody from '../src/gameBody.js'; 
- 
-const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'; 
- 
-const task = () => { 
-    const num = Math.round(Math.random() *50); 
-     
-    for (let i = 2; i <= Math.sqrt(num); i +=1) { 
-        if (num % 1 === 0) { 
-            return [num, 'no']; 
-        } 
-    } 
-    return [num, 'yes']; 
-}; 
- 
-gameBody(description,task);
+#!/usr/bin/env node
+import gameBody from '../src/gameBody.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const task = () => {
+    const num = Math.round(Math.random() * 50);
+
+    if (num <= 1) {
+        return [num, 'no'];
+    }
+
+    for (let i = 2; i <= Math.sqrt(num); i += 1) {
+        if (num % i === 0) {
+            return [num, 'no'];
+        }
+    }
+    return [num, 'yes'];
+};
+
+gameBody(description, task);
